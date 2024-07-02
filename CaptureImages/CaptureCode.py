ddb1260ee6b9f143465db52aa62fde3ecc1b0850
dhimitree <dhimitrip06@gmail.com>
2024-07-02 14:21:30 -0500

    wow

--- CaptureImages/CaptureCode.py
+++ CaptureImages/CaptureCode.py
@@ -77,6 +77,12 @@ while DoNextIteration:
             cv2.imshow(WINDOW_NAME, img)
             capture_and_save_image(CamDirOut+"w"+str(i)+SaveFormat)
 
+        imshowAndCapture(cap, img_pattern, delay=250):
+        cv2.imshow("", img_pattern)
+        cv2.waitKey(delay)
+        ret, img_frame = cap.read()
+        img_gray = cv2.cvtColor(img_frame, cv2.COLOR_BGR2GRAY)
+        return img_gray
         
 
     if DoNextIteration:
