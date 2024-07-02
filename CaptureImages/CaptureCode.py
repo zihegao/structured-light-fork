cdd48adb77f83d27a53631c85450a81ebb2bbbb9
pitchpipe <abrown.xf@gmail.com>
2024-07-02 13:49:19 -0500

    test

--- CaptureImages/CaptureCode.py
+++ CaptureImages/CaptureCode.py
@@ -70,9 +70,13 @@ while DoNextIteration:
         cv2.waitKey(0)
 
         # Capture
+        i = 0
         for img in imlist_posi_pat:
+            i+=1
             cv2.imshow(WINDOW_NAME, img)
-            capture_and_save_image(CamDirOut+imgnr+SaveFormat)
+            capture_and_save_image(CamDirOut+"w"+str(i)+SaveFormat)
+
+        
 
     if DoNextIteration:
         Popen(["python3 ConvertRawImage.py "+CamDirOut +" "+ SaveFormat +" "+ GrayCodeConverterPath], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
