20c611a30f73962201775d98817523cb79608b3f
pitchpipe <abrown.xf@gmail.com>
2024-07-03 14:22:20 -0500

    test

--- CaptureImages/CaptureCode.py
+++ CaptureImages/CaptureCode.py
@@ -18,7 +18,7 @@ WINDOW_NAME="phaseshift"
 
 def imShowAndCapture(cap, img_pattern, delay=250):
     cv2.imshow(WINDOW_NAME, img_pattern)
-    cv2.waitKey(delay)
+    cv2.waitKey(0)
     ret, img_frame = cap.read()
     img_gray = cv2.cvtColor(img_frame, cv2.COLOR_BGR2GRAY)    
     return img_gray
@@ -26,7 +26,7 @@ def imShowAndCapture(cap, img_pattern, delay=250):
 def SaveImageCV(images, filename, SaveFormat):
             i=0
             for img in images:
-                cv2.imwrite(filename + "_" + str(i) + SaveFormat, img)
+                cv2.imwrite(filename + str(i) + SaveFormat, img)
                 i+= 1
 
 #Capture Path: testcap
@@ -83,6 +83,9 @@ while DoNextIteration:
 
         cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
         cv2.imshow(WINDOW_NAME, imgToDisplay)
+        imlist_b_cap = [imShowAndCapture(cap, imgToDisplay)]
+        imgToDisplay[:,:] = 255
+        imlist_w_cap = [imShowAndCapture(cap, imgToDisplay)]
 
         # Capture
         imlist_posi_x_cap = [imShowAndCapture(cap, img) for img in imlist_posi_x_pat]
@@ -96,10 +99,13 @@ while DoNextIteration:
         imlist_posi_y_cap = [ imShowAndCapture(cap, img) for img in imlist_posi_y_pat]
         imlist_nega_y_cap = [ imShowAndCapture(cap, img) for img in imlist_nega_y_pat]
 
-        SaveImageCV(imlist_posi_x_cap, "h", SaveFormat)
-        SaveImageCV(imlist_nega_x_cap, "ih", SaveFormat)
-        SaveImageCV(imlist_posi_y_cap, "v", SaveFormat)
-        SaveImageCV(imlist_nega_y_cap, "iv", SaveFormat)
+        SaveImageCV(imlist_posi_x_cap, BaseOutputDir + "h", SaveFormat)
+        SaveImageCV(imlist_nega_x_cap, BaseOutputDir + "ih", SaveFormat)
+        SaveImageCV(imlist_posi_y_cap, BaseOutputDir + "v", SaveFormat)
+        SaveImageCV(imlist_nega_y_cap, BaseOutputDir + "iv", SaveFormat)
+        SaveImageCV(imlist_w_cap, BaseOutputDir + "w", SaveFormat)
+        SaveImageCV(imlist_b_cap, BaseOutputDir + "b", SaveFormat)
+
 
 
         
