198eaa3b4a09e2c9b259cce11a78bc9fdb51c2e0
EthanWilson14 <ethan.m.wilson@hotmail.com>
2025-06-02 10:22:01 -0500

    Update GrayCodesWindow.py

--- CaptureImages/GrayCodesWindow.py
+++ CaptureImages/GrayCodesWindow.py
@@ -1,11 +1,15 @@
+# code displays a sequence of Gray code pattern images using OpenCV
+# utilizes GrayImage class (from GrayImages.py) optionally applies remapping
+# press Enter when tab on other screen to start the fullscreen slideshow of encoded bit planes, then wait for process to finish
+
 import cv2
-from GrayImages import GrayImage
 import numpy as np
+from GrayImages import GrayImage
 
 gImg = GrayImage()
 WINDOW_NAME = "GrayCodesWindow"
 
-
+# disp Gray code in window, begins with prompt then iterates through all images
 def getImageIteration(firstIteration=True, map1=None, map2=None):
     if firstIteration:
         print("First It")
@@ -14,6 +18,7 @@ def getImageIteration(firstIteration=True, map1=None, map2=None):
         #cv2.imshow(WINDOW_NAME, imgToDisplay)
         #cv2.resizeWindow(WINDOW_NAME, imgToDisplay.shape[1], imgToDisplay.shape[0])
 
+        # displays the highest bit Gray code pattern
         imgToDisplay = gImg.getImage(gImg.num_bits-1)
         cv2.imshow(WINDOW_NAME, imgToDisplay)
     else:
@@ -21,19 +26,26 @@ def getImageIteration(firstIteration=True, map1=None, map2=None):
         imgToDisplay = np.array([[0]], dtype=np.uint8)
         cv2.imshow(WINDOW_NAME, imgToDisplay)
 
+    # waits for the key press
     k = cv2.waitKey(0)
 
+    # must be the enter (return) key
     if k != ord('\r'):
         return
+    
     if firstIteration:
+        #make full screen
         cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN);
+    # pause before starting sideshow
     k = cv2.waitKey(2000)
     for imgnr, imgToDisplay in gImg.getIterator():
         print(imgnr)
 
+        # aply remapping if calibration maps are given
         if map1 is not None and map2 is not None:
             imgToDisplay = cv2.remap(imgToDisplay, map1, map2, cv2.INTER_NEAREST)
 
+        # display the current Gray code image
         cv2.imshow(WINDOW_NAME, imgToDisplay)
         cv2.waitKey(10)
         yield imgnr
